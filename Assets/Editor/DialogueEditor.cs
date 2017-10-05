3955da5ff42198216392f9a9369b4c81f23cabc8
Andres Calandri <32347544+andresmcalandri@users.noreply.github.com>
2017-10-04 22:22:11 -0300

    Initial Commit
    Added the initial project.
    Added a window creator under DialogueEditor/Editor

---
+++ Assets/Editor/DialogueEditor.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+public class DialogueEditor : EditorWindow {
+    
+    [MenuItem("DialogueEditor/Editor")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(DialogueEditor));
+    }
+
+    List<Rect> controls = new List<Rect>();
+    void OnGUI()
+    {
+        if (GUILayout.Button("Create Window"))
+            CreateControl();
+
+        BeginWindows();
+
+        for (int i = 0; i < controls.Count; i++)
+        {
+            controls[i] = GUI.Window(i, controls[i], DoMyWindow, "Window_" + i);
+        }
+
+        EndWindows();
+    }
+
+    void DoMyWindow(int windowID)
+    {
+        if (GUI.Button(new Rect(190, 0, 10, 10), "X"))
+        {
+            DeleteControl(windowID);
+        }
+
+        if (GUI.Button(new Rect(10, 20, 100, 20), "Hello World"))
+            Debug.Log("Hello World");
+
+        GUI.DragWindow(new Rect(0, 0, 10000, 10000));
+    }
+
+    void CreateControl()
+    {
+        controls.Add(new Rect(0, 0, 200, 200));
+    }
+
+    void DeleteControl(int windowId)
+    {
+        controls.RemoveAt(windowId);
+    }
+}
