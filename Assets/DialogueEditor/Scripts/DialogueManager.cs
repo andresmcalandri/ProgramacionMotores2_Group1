87994ece189ecbc53fcb383ba4825f1693fb3ced
Raykaster <obrasdemarcos@gmail.com>
2017-10-18 18:47:31 -0300

    Test Scene, DialogueManager

---
+++ Assets/DialogueEditor/Scripts/DialogueManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour {
+
+    //Reference to the first dialogue
+    public Dialogue initialDialogue;
+    Dialogue currentDialogue;
+
+	void Start ()
+    {
+        if (initialDialogue != null) PlayDialogue(initialDialogue);
+		
+	}
+	
+
+    void PlayDialogue(Dialogue dialogue)
+    {
+        currentDialogue = dialogue;
+        //The funcion should load the text of each node in the Dialogue, including the character's name
+        //In case it's a question the system should spawn as many buttons as necesary and link to the required node
+    }
+
+
+}
