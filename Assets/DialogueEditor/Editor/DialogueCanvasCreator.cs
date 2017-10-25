09139031b36969d18d8f42328a37696917f1c6b5
Raykaster <obrasdemarcos@gmail.com>
2017-10-25 00:49:27 -0300

    Load text content on the Dialogue Cavnas, added a way to instanciate the Dialogue Cavnas from a prefab using the Editor Window

---
+++ Assets/DialogueEditor/Editor/DialogueCanvasCreator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class DialogueCanvasCreator : EditorWindow
+{
+    [MenuItem("DialogueEditor/Create Dialogue Cavnas")]
+    static void Create()
+    {
+        GameObject dialogueCavnas = (GameObject)Resources.Load("Prefabs/DialogueCanvas");
+        dialogueCavnas.name = "Dialogue Cavnas";
+        Instantiate(dialogueCavnas);
+
+    }
+}
--- Assets/DialogueEditor/Scripts/DialogueManager.cs
+++ Assets/DialogueEditor/Scripts/DialogueManager.cs
@@ -1,25 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour {
 
     //Reference to the first dialogue
-    public Dialogue initialDialogue;
-    Dialogue currentDialogue;
+    public DialogueItem initialDialogue;
+    DialogueItem currentDialogue;
 
-	void Start ()
+    public Text characterName;
+    public Text dialogueText;
+
+    public Button optionA;
+    public Button optionB;
+
+
+    void Start ()
     {
         if (initialDialogue != null) PlayDialogue(initialDialogue);
-		
+        //if (GetComponent<Canvas>().worldCamera == null) GetComponent<Canvas>().worldCamera = GameObject.FindObjectOfType(Camera);
 	}
 	
 
-    void PlayDialogue(Dialogue dialogue)
+    void PlayDialogue(DialogueItem dialogue)
     {
         currentDialogue = dialogue;
         //The funcion should load the text of each node in the Dialogue, including the character's name
         //In case it's a question the system should spawn as many buttons as necesary and link to the required node
+
+        //TODO: Add characterName to the DialogueItem
+        characterName.text = "";
+        dialogueText.text = dialogue.name;
+
+        if(dialogue.answers.Count==0)
+        {
+            optionA.gameObject.SetActive(false);
+            optionB.gameObject.SetActive(false);
+        }
+
     }
 
 
